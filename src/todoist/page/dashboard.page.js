1d62d127eda9db39470eba2ed48a438c9ea54c91
Osvaldo <jose.barajas@wizeline.com>
2022-02-24 18:08:23 -0600

    Updates for tasts tests

--- src/todoist/page/dashboard.page.js
+++ src/todoist/page/dashboard.page.js
@@ -1,11 +1,24 @@
-import { Selector } from "testcafe";
+import { Selector, t } from "testcafe";
 
 class DashboardPage{
     constructor(){
-        this.mainTitle         = Selector('h1 > span.simple_content');
-        this.addTaskButton     = Selector('button.empty-state-button');
-        this.addTaskPlusButton = Selector('button.plus_add_button');
-        this.taskList          = Selector('div.task_content');
+        this.mainTitle          = Selector('h1 > span.simple_content');
+        this.addTaskButton      = Selector('button.empty-state-button');
+        this.addTaskPlusButton  = Selector('button.plus_add_button');
+        this.taskList           = Selector('div.task_content');
+        this.completeTaskButton = Selector('div.task_checkbox__circle');
+    }
+
+    async isTaskDisplayed(taskTitle){
+        return this.taskList.withText(taskTitle).exists;
+    }
+
+    async marckAsConcludedTask(){
+        while(await this.completeTaskButton.nth(0).exists){
+            await t
+                .click(this.completeTaskButton.nth(0))
+                .wait(1000);
+        }
     }
 }
 
